baba71b45f3b7025eab292619d4244fa0406e2ff
M. Jirik <miroslav.jirik@gmail.com>
2014-10-07 15:09:00 +0200

    separated run funcion from command line parser

--- py/computation/step_calcchains_serial_tobinary_filter_proc_lisa.py
+++ py/computation/step_calcchains_serial_tobinary_filter_proc_lisa.py
@@ -43,9 +43,9 @@ logging_level = 2;
 # 3 = many many details
 
 def log(n, l):
-	if __name__=="__main__" and n <= logging_level:
-		for s in l:
-			print "Log:", s;
+    if __name__=="__main__" and n <= logging_level:
+        for s in l:
+            print "Log:", s;
 
 # ------------------------------------------------------------
 # Configuration parameters
@@ -59,25 +59,25 @@ BIN_EXTENSION = ".bin"
 # ------------------------------------------------------------
 
 def invertIndex(nx,ny,nz):
-	nx,ny,nz = nx+1,ny+1,nz+1
-	def invertIndex0(offset):
-		a0, b0 = offset / nx, offset % nx
-		a1, b1 = a0 / ny, a0 % ny
-		a2, b2 = a1 / nz, a1 % nz
-		return b0,b1,b2
-	return invertIndex0
+    nx,ny,nz = nx+1,ny+1,nz+1
+    def invertIndex0(offset):
+        a0, b0 = offset / nx, offset % nx
+        a1, b1 = a0 / ny, a0 % ny
+        a2, b2 = a1 / nz, a1 % nz
+        return b0,b1,b2
+    return invertIndex0
 
 def countFilesInADir(directory):
-	return len(os.walk(directory).next()[2])
+    return len(os.walk(directory).next()[2])
 
 def isArrayEmpty(arr):
-	return all(e == 0 for e in arr)
+    return all(e == 0 for e in arr)
 
 # ------------------------------------------------------------
 def writeOffsetToFile(file, offsetCurr):
-	file.write( struct.pack('>I', offsetCurr[0]) )
-	file.write( struct.pack('>I', offsetCurr[1]) )
-	file.write( struct.pack('>I', offsetCurr[2]) )
+    file.write( struct.pack('>I', offsetCurr[0]) )
+    file.write( struct.pack('>I', offsetCurr[1]) )
+    file.write( struct.pack('>I', offsetCurr[2]) )
 # ------------------------------------------------------------
 
 def read_pkl_by_block(datap, startImage, endImage, centroidsCalc):
@@ -177,8 +177,8 @@ def computeChainsThread(
             # import ipdb; ipdb.set_trace() #  noqa BREAKPOINT
             # theColors = theColors.reshape(1,colorNumber)
             # if (sorted(theColors[0]) != saveTheColors):
-            #	log(1, [ "Error: colorNumber have changed"] )
-            #	sys.exit(2)
+            #    log(1, [ "Error: colorNumber have changed"] )
+            #    sys.exit(2)
 
             log(2, [ "Working task: " +str(startImage) + "-" + str(endImage) + " [comp loop]" ])
             for xBlock in xrange(imageHeight/imageDx):
@@ -245,38 +245,40 @@ def computeChainsThread(
 
 processRes = []
 def collectResult(resData):
-	processRes.append(resData)
+    processRes.append(resData)
 
-def startComputeChains(imageHeight,imageWidth,imageDepth,
-        imageDx,imageDy,imageDz,
-        Nx,Ny,Nz, calculateout,BORDER_FILE,
-        centroidsCalc, colorIdx,datap,DIR_O):
-	beginImageStack = 0
-	endImage = beginImageStack
+def startComputeChains(
+    imageHeight,imageWidth,imageDepth,
+    imageDx,imageDy,imageDz,
+    Nx,Ny,Nz, calculateout,BORDER_FILE,
+    centroidsCalc, colorIdx,datap,DIR_O):
 
-	saveTheColors = centroidsCalc
-        print 'ccalc: ', centroidsCalc
-	log(2, [ centroidsCalc ])
-	saveTheColors = np.array( sorted(saveTheColors.reshape(1,len(centroidsCalc))[0]), dtype=np.int )
-	log(2, [ saveTheColors ])
-	# print str(imageHeight) + '-' + str(imageWidth) + '-' + str(imageDepth)
-	# print str(imageDx) + '-' + str(imageDy) + '-' + str(imageDz)
-	# print str(Nx) + '-' + str(Ny) + '-' + str(Nz)
-	returnValue = 2
+    beginImageStack = 0
+    endImage = beginImageStack
 
-	processPool = max(1, multiprocessing.cpu_count()/2)
-	log(2, [ "Starting pool with: " + str(processPool) ])
+    saveTheColors = centroidsCalc
+    print 'ccalc: ', centroidsCalc
+    log(2, [ centroidsCalc ])
+    saveTheColors = np.array( sorted(saveTheColors.reshape(1,len(centroidsCalc))[0]), dtype=np.int )
+    log(2, [ saveTheColors ])
+    # print str(imageHeight) + '-' + str(imageWidth) + '-' + str(imageDepth)
+    # print str(imageDx) + '-' + str(imageDy) + '-' + str(imageDz)
+    # print str(Nx) + '-' + str(Ny) + '-' + str(Nz)
+    returnValue = 2
 
-	try:
-		pool = multiprocessing.Pool(processPool)
-		log(2, [ 'Start pool' ])
+    processPool = max(1, multiprocessing.cpu_count()/2)
+    log(2, [ "Starting pool with: " + str(processPool) ])
 
-		for j in xrange(imageDepth/imageDz):
-			startImage = endImage
-			endImage = startImage + imageDz
-			log(2, [ "Added task: " + str(j) + " -- (" + str(startImage) + "," + str(endImage) + ")" ])
+    try:
+        pool = multiprocessing.Pool(processPool)
+        log(2, [ 'Start pool' ])
 
-			pool.apply_async(
+        for j in xrange(imageDepth/imageDz):
+            startImage = endImage
+            endImage = startImage + imageDz
+            log(2, [ "Added task: " + str(j) + " -- (" + str(startImage) + "," + str(endImage) + ")" ])
+
+            pool.apply_async(
                             computeChainsThread,
                             args = (startImage,endImage,imageHeight,imageWidth,
                                 imageDx,imageDy,imageDz, Nx,Ny,Nz, calculateout,
@@ -284,19 +286,19 @@ def startComputeChains(imageHeight,imageWidth,imageDepth,
                                 colorIdx,datap,DIR_O, ),
                             callback = collectResult)
 
-		log(2, [ "Waiting for completion..." ])
-		pool.close()
-		pool.join()
+        log(2, [ "Waiting for completion..." ])
+        pool.close()
+        pool.join()
 
-		log(1, [ "Completed: " + str(processRes) ])
-		if (sum(processRes) == 0):
-			returnValue = 0
-	except:
-		exc_type, exc_value, exc_traceback = sys.exc_info()
-		lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
-		log(1, [ "Error: " + ''.join('!! ' + line for line in lines) ])  # Log it or whatever here
+        log(1, [ "Completed: " + str(processRes) ])
+        if (sum(processRes) == 0):
+            returnValue = 0
+    except:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
+        log(1, [ "Error: " + ''.join('!! ' + line for line in lines) ])  # Log it or whatever here
 
-	return returnValue
+    return returnValue
 
 def segmentation_from_data3d(datap):
     print "automatic segmentation"
@@ -308,138 +310,156 @@ def segmentation_from_data3d(datap):
     return datap
 
 def runComputation(imageDx,imageDy,imageDz, coloridx,calculateout,
-        V,FV, input_pkl_file, best_slice, BORDER_FILE, DIR_O):
+    V,FV, input_pkl_file, BORDER_FILE, DIR_O):
 
-        dr = datareader.DataReader()
-        datap = dr.Get3DData(input_pkl_file, qt_app=None, dataplus_format=True,
-                             gui=False)
-        if 'segmentation' not in datap.keys():
-            datap = segmentation_from_data3d(datap)
-        print '###################################'
-        segmentation = datap['segmentation'].astype(np.uint8)
+    dr = datareader.DataReader()
+    datap = dr.Get3DData(input_pkl_file, qt_app=None, dataplus_format=True,
+                            gui=False)
+    if 'segmentation' not in datap.keys():
+        datap = segmentation_from_data3d(datap)
+    print '###################################'
+    segmentation = datap['segmentation'].astype(np.uint8)
 
-        #segmentation = datap['segmentation'][::5,::5,::5]
-        #segmentation = datap['segmentation'][300:330,300:350,300:350]
-        # datap['segmentation'] = (segmentation ==  1).astype(np.uint8)
-        # import ipdb; ipdb.set_trace() #  noqa BREAKPOINT
-        # hack kvuli barvam
-        segmentation[0,0,0] = 0
-        segmentation[0,0,1] = 1
-        datap['segmentation'] = segmentation
-        print np.unique(datap['segmentation'])
-        print datap.keys()
-        print datap['segmentation'].shape
-        imageHeight,imageWidth = datap['segmentation'][:,:,:].shape[1:3]
-        #getImageData(INPUT_DIR+str(BEST_IMAGE)+PNG_EXTENSION)
-	#imageDepth = countFilesInADir(INPUT_DIR)
-	imageDepth = datap['segmentation'].shape[0]
-	Nx,Ny,Nz = imageHeight/imageDx, imageWidth/imageDx, imageDepth/imageDz
-	returnValue = 2
+    #segmentation = datap['segmentation'][::5,::5,::5]
+    #segmentation = datap['segmentation'][300:330,300:350,300:350]
+    # datap['segmentation'] = (segmentation ==  1).astype(np.uint8)
+    # import ipdb; ipdb.set_trace() #  noqa BREAKPOINT
+    # hack kvuli barvam
+    segmentation[0,0,0] = 0
+    segmentation[0,0,1] = 1
+    datap['segmentation'] = segmentation
+    print np.unique(datap['segmentation'])
+    print datap.keys()
+    print datap['segmentation'].shape
+    imageHeight,imageWidth = datap['segmentation'][:,:,:].shape[1:3]
+    #getImageData(INPUT_DIR+str(BEST_IMAGE)+PNG_EXTENSION)
+    #imageDepth = countFilesInADir(INPUT_DIR)
+    imageDepth = datap['segmentation'].shape[0]
+    Nx,Ny,Nz = imageHeight/imageDx, imageWidth/imageDx, imageDepth/imageDz
+    returnValue = 2
 
-	try:
-		# pixelCalc, centroidsCalc = centroidcalc(INPUT_DIR, BEST_IMAGE, colors)
+    try:
+        # pixelCalc, centroidsCalc = centroidcalc(INPUT_DIR, BEST_IMAGE, colors)
                 # centroidsCalc = np.array([0, 1])
-                centroidsCalc = np.unique(datap['segmentation'])
-		returnValue = startComputeChains(
+        centroidsCalc = np.unique(datap['segmentation'])
+        returnValue = startComputeChains(
                         imageHeight,imageWidth,imageDepth,
                         imageDx,imageDy,imageDz, Nx,Ny,Nz,
                         calculateout,BORDER_FILE,
                         centroidsCalc, coloridx,
                         datap,DIR_O)
-	except:
-		exc_type, exc_value, exc_traceback = sys.exc_info()
-		lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
-		log(1, [ "Error: " + ''.join('!! ' + line for line in lines) ])  # Log it or whatever here
-		returnValue = 2
+    except:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
+        log(1, [ "Error: " + ''.join('!! ' + line for line in lines) ])  # Log it or whatever here
+        returnValue = 2
 
-	sys.exit(returnValue)
+    sys.exit(returnValue)
 
 def main(argv):
-	ARGS_STRING = 'Args: -r -b <borderfile> -x <borderX> -y <borderY> -z <borderZ> -i <inputdirectory> -c <colors> -d <coloridx> -o <outputdir> -q <bestimage>'
+    ARGS_STRING = 'Args: -r -b <borderfile> -x <borderX> -y <borderY> -z <borderZ> -i <inputdirectory> -c <colors> -d <coloridx> -o <outputdir> -q <bestimage>'
 
-	try:
-		opts, args = getopt.getopt(argv,"rb:x:y:z:i:c:d:o:q:")
-	except getopt.GetoptError:
-		print ARGS_STRING
-		sys.exit(2)
+    try:
+        opts, args = getopt.getopt(argv,"rb:x:y:z:i:c:d:o:q:")
+    except getopt.GetoptError:
+        print ARGS_STRING
+        sys.exit(2)
 
-	nx = ny = nz = imageDx = imageDy = imageDz = 64
-	colors = 2
-	coloridx = 0
+    nx = ny = nz = imageDx = imageDy = imageDz = 64
+    colors = 2
+    coloridx = 0
 
-	mandatory = 5
-	calculateout = False
-	#Files
-	BORDER_FILE = 'bordo3.json'
-	BEST_IMAGE = ''
-	DIR_IN = ''
-	DIR_O = ''
+    mandatory = 5
+    calculateout = False
+    #Files
+    BORDER_FILE = 'bordo3.json'
+    BEST_IMAGE = ''
+    DIR_IN = ''
+    DIR_O = ''
 
-	for opt, arg in opts:
-		if opt == '-x':
-			nx = ny = nz = imageDx = imageDy = imageDz = int(arg)
-			mandatory = mandatory - 1
-		elif opt == '-y':
-			ny = nz = imageDy = imageDz = int(arg)
-		elif opt == '-z':
-			nz = imageDz = int(arg)
-		elif opt == '-r':
-			calculateout = True
-		elif opt == '-i':
-			DIR_IN = arg + '/'
-			mandatory = mandatory - 1
-		elif opt == '-b':
-			BORDER_FILE = arg
-			mandatory = mandatory - 1
-		elif opt == '-o':
-			mandatory = mandatory - 1
-			DIR_O = arg
-		elif opt == '-c':
-			# mandatory = mandatory - 1
-			colors = int(arg)
-		elif opt == '-d':
-			mandatory = mandatory - 1
-			coloridx = int(arg)
-		elif opt == '-q':
-			BEST_IMAGE = int(arg)
-			#BEST_IMAGE = 10
+    for opt, arg in opts:
+        if opt == '-x':
+            nx = ny = nz = imageDx = imageDy = imageDz = int(arg)
+            mandatory = mandatory - 1
+        elif opt == '-y':
+            ny = nz = imageDy = imageDz = int(arg)
+        elif opt == '-z':
+            nz = imageDz = int(arg)
+        elif opt == '-r':
+            calculateout = True
+        elif opt == '-i':
+            DIR_IN = arg + '/'
+            mandatory = mandatory - 1
+        elif opt == '-b':
+            BORDER_FILE = arg
+            mandatory = mandatory - 1
+        elif opt == '-o':
+            mandatory = mandatory - 1
+            DIR_O = arg
+        elif opt == '-c':
+            # mandatory = mandatory - 1
+            colors = int(arg)
+        elif opt == '-d':
+            mandatory = mandatory - 1
+            coloridx = int(arg)
+        elif opt == '-q':
+            BEST_IMAGE = int(arg)
+            #BEST_IMAGE = 10
 
-	if mandatory != 0:
-		print 'Not all arguments where given'
-		print ARGS_STRING
-		sys.exit(2)
+    if mandatory != 0:
+        print 'Not all arguments where given'
+        print ARGS_STRING
+        sys.exit(2)
 
-	# if (coloridx >= colors):
-	# 	print 'Not all arguments where given (coloridx >= colors)'
-	# 	print ARGS_STRING
-	# 	sys.exit(2)
+    calcchains_main(
+        nx, ny, nz,
+        calculateout,
+        DIR_IN,
+        BORDER_FILE,
+        DIR_O,
+        colors,
+        coloridx
+        )
 
-	def ind(x,y,z): return x + (nx+1) * (y + (ny+1) * (z))
+def calcchains_main(
+    nx, ny, nz,
+    calculateout,
+    DIR_IN,
+    BORDER_FILE,
+    DIR_O,
+    colors,
+    coloridx
+    ):
+    # if (coloridx >= colors):
+    #     print 'Not all arguments where given (coloridx >= colors)'
+    #     print ARGS_STRING
+    #     sys.exit(2)
+
+    def ind(x,y,z): return x + (nx+1) * (y + (ny+1) * (z))
 
 
-	chunksize = nx * ny + nx * nz + ny * nz + 3 * nx * ny * nz
-	V = [[x,y,z] for z in xrange(nz+1) for y in xrange(ny+1) for x in xrange(nx+1) ]
+    chunksize = nx * ny + nx * nz + ny * nz + 3 * nx * ny * nz
+    V = [[x,y,z] for z in xrange(nz+1) for y in xrange(ny+1) for x in xrange(nx+1) ]
 
-	v2coords = invertIndex(nx,ny,nz)
+    v2coords = invertIndex(nx,ny,nz)
 
 
         # mj
         # construction of vertex grid
-	FV = []
-	for h in xrange(len(V)):
-		x,y,z = v2coords(h)
-		if (x < nx) and (y < ny): FV.append([h,ind(x+1,y,z),ind(x,y+1,z),ind(x+1,y+1,z)])
-		if (x < nx) and (z < nz): FV.append([h,ind(x+1,y,z),ind(x,y,z+1),ind(x+1,y,z+1)])
-		if (y < ny) and (z < nz): FV.append([h,ind(x,y+1,z),ind(x,y,z+1),ind(x,y+1,z+1)])
+    FV = []
+    for h in xrange(len(V)):
+        x,y,z = v2coords(h)
+        if (x < nx) and (y < ny): FV.append([h,ind(x+1,y,z),ind(x,y+1,z),ind(x+1,y+1,z)])
+        if (x < nx) and (z < nz): FV.append([h,ind(x+1,y,z),ind(x,y,z+1),ind(x+1,y,z+1)])
+        if (y < ny) and (z < nz): FV.append([h,ind(x,y+1,z),ind(x,y,z+1),ind(x,y+1,z+1)])
 
-	print 'imDx', imageDx, imageDy, imageDz
-        print 'colors', colors, coloridx
-        print 'calc', calculateout
-        print 'V.len: ', len(V), ' V[0:4]: ', V[0:4]
-        print 'FV.len: ', len(FV), 'FV[0:4]: ', FV[0:4]
-        print 'dirl', DIR_IN, BEST_IMAGE, BORDER_FILE
-        print 'diro ', DIR_O
-        runComputation(imageDx, imageDy, imageDz, coloridx, calculateout, V, FV, DIR_IN, BEST_IMAGE, BORDER_FILE, DIR_O)
+    print 'colors', colors, coloridx
+    print 'calc', calculateout
+    print 'V.len: ', len(V), ' V[0:4]: ', V[0:4]
+    print 'FV.len: ', len(FV), 'FV[0:4]: ', FV[0:4]
+    print 'dirl', DIR_IN, BORDER_FILE
+    print 'diro ', DIR_O
+    runComputation(nx, ny, nz, coloridx, calculateout, V, FV, DIR_IN, BORDER_FILE, DIR_O)
 
 if __name__ == "__main__":
-	main(sys.argv[1:])
+    main(sys.argv[1:])
