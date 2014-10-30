e1f6b55a5fd90cd5fc5bab79b6fa08f962af8079
M. Jirik <miroslav.jirik@gmail.com>
2014-10-30 14:44:00 +0100

    smooth test returned

--- tests/smoothing_test.py
+++ tests/smoothing_test.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # import funkcí z jiného adresáře
-import sys
+# import sys
 import os.path
 
 path_to_script = os.path.dirname(os.path.abspath(__file__))
@@ -12,6 +12,7 @@ import unittest
 
 import numpy as np
 import larVolumeToObj.computation.laplacianSmoothing as ls
+import larVolumeToObj.computation.smooth as smooth
 
 
 class SmoothingTest(unittest.TestCase):
@@ -114,5 +115,25 @@ class SmoothingTest(unittest.TestCase):
         nv = ls.makeSmoothing(vertexes, faces)
         print nv
 
+    def test_smooth_mira(self):
+        vertexes = np.array([
+            [1, 3, 6],
+            [2, 2, 5],
+            [2, 2, 4],
+            [2, 1, 6],
+            [1, 3, 5]])
+
+        faces = np.array([
+            [1, 2, 3],
+            [1, 2, 4],
+            [2, 4, 5],
+            [4, 5, 2]])
+
+        expected_vertex = np.array(
+            [1.6, 2.2, 5.2])
+
+        new_vertex = smooth.smoothPositionOfVertex(vertexes, faces, 2)
+
+        self.assertAlmostEqual(0, np.sum(new_vertex - expected_vertex))
 if __name__ == "__main__":
     unittest.main()
