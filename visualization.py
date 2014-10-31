36b9f282c55b77d509b04fe95d82a4b88e8b24c2
M. Jirik <miroslav.jirik@gmail.com>
2014-10-31 11:15:27 +0100

    added caller for visualization

---
+++ visualization.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2014 mjirik <mjirik@mjirik-Latitude-E6520>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+
+import logging
+logger = logging.getLogger(__name__)
+import larVolumeToObj.computation.visualization as visualization
+
+
+if __name__ == "__main__":
+    visualization.main()
