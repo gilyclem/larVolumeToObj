6aab2c5623791267dee2b6242a14c3a5f56f7169
M. Jirik <miroslav.jirik@gmail.com>
2014-12-04 17:32:47 +0100

    slice points to vol fixed

--- larVolumeToObj/computation/objToVolume.py
+++ larVolumeToObj/computation/objToVolume.py
@@ -17,10 +17,51 @@ from fileio import readFile, writeFile
 from scipy.spatial import Delaunay
 import numpy as np
 
+
+def points_to_volume_3D(data3d, points):
+    """
+    Not fixed yet. Should be better then slice version
+    """
+    # hack move one point in next slice to make non planar object
+    points[0, 2] += 1
+    points[-1, 2] += -1
+
+
+    hull = Delaunay(points)
+    X, Y, Z = np.mgrid[:size[0],:size[1], :size[2]]
+    grid = np.vstack([X.ravel(), Y.ravel(), Z.ravel()]).T
+    simplex = hull.find_simplex(grid)
+    fill = grid[simplex >=0,:]
+    fill = (fill[:,0], fill[:,1], fill[:,2])
+    contours = np.zeros(data.shape, np.int8)
+    contours[fill] = 1
+
+def points_to_volume_slice(data3d, points):
+    """
+    Only planar points can be used
+    """
+    # hack move one point in next slice to make non planar object
+    z = points[0, 2]
+    points = points[:, :2]
+
+    hull = Delaunay(points)
+    X, Y = np.mgrid[:data3d.shape[0], :data3d.shape[1]]
+    grid = np.vstack([X.ravel(), Y.ravel()]).T
+    simplex = hull.find_simplex(grid)
+    fill = grid[simplex >=0,:]
+    fill = (fill[:,0], fill[:,1])
+    contours = np.zeros(data3d.shape, np.int8)
+    contours[fill] = 1
+
+    import ipdb; ipdb.set_trace() #  noqa BREAKPOINT
+
+
+
 def read_files_and_make_labeled_image(filesmask):
     int_multiplicator = 1000
-    data_offset = [5600,700,100]
+    data_offset = [5600,6900,100]
     size = [300, 300, 300]
+    data3d = np.zeros(size)
 
     Vraw, Fraw = readFile(filesmask)
     
@@ -48,9 +89,13 @@ def read_files_and_make_labeled_image(filesmask):
 
 
     for slicelevel in np.unique(Vint[:, 2]):
-        selected = Vint[Vint[:, 2] == slicelevel, :]
-        import ipdb; ipdb.set_trace() #  noqa BREAKPOINT
+        points = Vint[Vint[:, 2] == slicelevel, :]
+        t = False 
 
+        if points.shape[0] > 2:
+            points_to_volume_slice(data3d, points)
+
+            import ipdb; ipdb.set_trace() #  noqa BREAKPOINT
         
 
 
