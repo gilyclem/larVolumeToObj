da93b30d93dca708674daa3e6b4e762cdb4afa35
M. Jirik <miroslav.jirik@gmail.com>
2015-02-03 16:21:07 +0100

    not importing all automatically

--- larVolumeToObj/computation/__init__.py
+++ larVolumeToObj/computation/__init__.py
@@ -6,19 +6,19 @@ __all__ = [
     'step_remove_boxes_iner_faces', 'step_squaremesh', 'step_triangularmesh',
     'visualization', 'import_library'
 ]
-import fileio
-import laplacianSmoothing
-import lar
-import matrixutil_accel
-import matrixutil_no_accel
-import pklzToSmoothObj
-import smooth
-import step_calcchains_serial_tobinary_filter_proc_lisa
-import step_generatebordermtx
-import step_loadmodel
-import step_mergemesh
-import step_remove_boxes_iner_faces
-import step_squaremesh
-import step_triangularmesh
-import visualization
-import import_library
+# import fileio
+# import laplacianSmoothing
+# import lar
+# import matrixutil_accel
+# import matrixutil_no_accel
+# import pklzToSmoothObj
+# import smooth
+# import step_calcchains_serial_tobinary_filter_proc_lisa
+# import step_generatebordermtx
+# import step_loadmodel
+# import step_mergemesh
+# import step_remove_boxes_iner_faces
+# import step_squaremesh
+# import step_triangularmesh
+# import visualization
+# import import_library
